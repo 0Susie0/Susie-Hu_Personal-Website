2d225be376faa40e46f2c656b4b254d162664fbc
Susie Hu <421686577@qq.com>
2025-07-03 16:10:24 +1000

    Fix clicking issue on header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -9,8 +9,8 @@ function Header() {
         <nav aria-label="Main Navigation">
           <ul style={{ listStyle: 'none', display: 'flex', gap: '0.5rem', margin: 0, padding: 0 }}>
             <li><a href="#about">About</a></li>
-            <li><a href="#Portfolio">Portfolio</a></li>
-            <li><a href="#Skills">Skills</a></li>
+            <li><a href="#portfolio">Portfolio</a></li>
+            <li><a href="#skills">Skills</a></li>
             <li><a href="#education-timeline">Education & Experience</a></li>
             <li><a href="#contact">Contact</a></li>
           </ul>
